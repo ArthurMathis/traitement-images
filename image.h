ef1a12e7ad33d892714aeb6771816277fbafa022
ArthurMathis <mathis.arthur68@gmail.com>
2023-09-13 08:35:59 +0200

    Ajout de la documentation

--- image.h
+++ image.h
@@ -7,18 +7,39 @@
 
 #include <string>
 
+/**
+ * @brief Structure représentant un pixel
+ */
 struct pixel{
     unsigned red, green, blue;
 };
 
+/**
+ * @brief Class représentant une image de format .ppm
+ */
 class image {
 public:
     image(std::string source);
 
+    /**
+     * @brief Méthode récupérerant la source de l'image
+     * @return
+     */
     std::string getSource() const;
+    /*
+     * @brief Méthode changeant la source d'une image
+     */
     void setSource(std::string source);
 
+    /**
+     * @brief Méthode générant depuis  l'image, une image en noir et blanc
+     * @param source
+     */
     void NiveauxDeGris(std::string source) const;
+    /**
+     * @brief Méthode générant depuis l'image, une image en rouge et gris
+     * @param source
+     */
     void RougeEtGris(std::string source) const;
 
 private:
